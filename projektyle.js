2b6925e27e2e95cf4cf2aa64bc28ac14f3298bba
Manan Shah <manan.shah.777@gmail.com>
2014-05-10 20:19:03 -0700

    Create projektyle.js

---
+++ projektyle.js
@@ -0,0 +1,41 @@
+/** 
+ * Projektyle JS Code
+ * Rank: 2182
+ * Strategy: 
+ *  Fire shots in a "v" shaped formation, encompassing the enemy robot and 
+ *  preventing possibility of escape. Clone at the beginning of the match
+ *  and disappear if health is less than 50. 
+ *
+ * Author: Manan
+ * Version: 05102014
+**/
+function Robot(projektyle) {
+    projektyle.rotateCannon(60);
+  	projektyle.clone();
+}
+
+var t = 0;
+
+Robot.prototype.onScannedRobot = function(ev) {
+    var projektyle = ev.robot;
+    var enemy = ev.scannedRobot;
+    //an actual enemy, NOT an ally
+    if (enemy.id !== projektyle.parentId && enemy.parentId !== projektyle.id) {
+        projektyle.fire();
+        projektyle.rotateCannon(35);
+        projektyle.fire();
+    }
+};
+
+Robot.prototype.onIdle = function(ev) {
+    var projektyle = ev.robot;
+    projektyle.ahead(10);
+    projektyle.turn(-45);
+    t += 1;
+    if (t == 5) {
+        projektyle.ahead(150);
+        t = 0;
+    }
+    if(projektyle.life < 60) projektyle.disappear();
+    //if (projektyle.life < 60) projektyle.clone();
+};
