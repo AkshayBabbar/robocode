a341e6ed679daedb5e852a42ee1e94658286519f
Manan Shah <manan.shah.777@gmail.com>
2014-07-04 09:40:34 -0700

    Update vektor.js

--- vektor.js
+++ vektor.js
@@ -1,21 +1,22 @@
 
 /**
  * Vektor Robot Code
- * Version 5112014
- * Rank: 226
- * Author: Manan
+ * Highest Rank: 226
+ * Version 7042014
 **/
 
 var t = 0;
 var Robot = function(vektor) {
-  vektor.rotateCannon(90); 
+  if(vektor.parentId === null) 
+    vektor.rotateCannon(90); 
 };
- 
+  
 Robot.prototype.onIdle = function(ev) {
   var vektor = ev.robot;
   vektor.clone();   
   //find the clone
   if (vektor.parentId !== null) {
+    /*
     vektor.ahead(10);
     vektor.turn(-45);
     t += 1;
@@ -24,11 +25,21 @@ Robot.prototype.onIdle = function(ev) {
         t = 0;
     }
     if(vektor.life < 60) {
-	vektor.disappear();
-	vektor.ahead(200);
-    }	
-  }
-  vektor.ahead(1000);  
+			vektor.disappear();
+			vektor.ahead(200);
+    }	*/
+    vektor.move(25);
+    vektor.turn(-30);
+    for(var t = 1; t < 30; t++) {
+      vektor.turn(20);
+      vektor.move(20);
+    }
+    for(var t = 1; t < 20; t++) {
+      vektor.turn(-30);
+      vektor.move(20);
+    }
+  } 
+  else vektor.ahead(1000);
 };
  
 Robot.prototype.onScannedRobot = function(ev) {
@@ -36,19 +47,18 @@ Robot.prototype.onScannedRobot = function(ev) {
   var enemy = ev.scannedRobot;
   if(enemy.id !== vektor.parentId && enemy.parentId !== vektor.id) {
     vektor.fire(1);
-    if (ev.robot.parentId !== null) {
-	vektor.fire();
-      	vektor.fire();
-        vektor.rotateCannon(35);
-        vektor.fire();
-      	vektor.fire();
+    if (vektor.parentId !== null) {
+				vektor.stop();
+        vektor.fire(2);
+        vektor.move(50);
+        vektor.fire(2);
     }
     else {
-      vektor.rotateCannon(-20);
-      vektor.fire(2);
-      vektor.rotateCannon(20);
-      vektor.fire(2);
-    }
+        vektor.rotateCannon(-20);
+        vektor.fire(3);
+        vektor.rotateCannon(20);
+        vektor.fire(3);
+    }  
   }  
 };
  
@@ -58,7 +68,7 @@ Robot.prototype.onWallCollision = function(ev) {
   	vektor.turn(ev.bearing + 90); 
   }
   else {
-    vektor.back(100);
+    vektor.back(20);
     vektor.rotateCannon(-180); 
     vektor.turn(ev.bearing - 110); 
   }
@@ -66,13 +76,23 @@ Robot.prototype.onWallCollision = function(ev) {
  
 Robot.prototype.onRobotCollision = function(ev) {
   var vektor = ev.robot;
-  vektor.back(100); 
-  vektor.turn(90);
+  var enemy = ev.collidedRobot;
+  if(vektor.parentId !== null) {
+    if(enemy.id !== vektor.parentId && enemy.id !== vektor.id) {
+      vektor.stop();
+      vektor.turn(ev.bearing * 1.3);
+      vektor.turn(ev.bearing * -1.3);
+    }
+  }
+  else {
+    vektor.back(100); 
+    vektor.turn(90);
+  }
 };
  
 Robot.prototype.onHitByBullet = function(ev) {
   var vektor = ev.robot;
-  vektor.disappear();
+  //vektor.disappear();
   if (vektor.parentId !== null) {   
     vektor.stop();
     vektor.rotateCannon(ev.bearing);
